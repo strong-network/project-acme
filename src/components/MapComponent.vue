cc4886b7bccdfe47c8d3c1911c174c5673b06267
Bastian Nanchen <bastian.nanchen@cloud.com>
2025-05-02 11:00:16 +0200

    Update dependencies and small nits

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,11 +1,16 @@
 <template>
-  <GoogleMap :api-key="apiKey" style="width: 100%; height: 500px" :center="center" :zoom="15">
+  <GoogleMap
+    :api-key="apiKey"
+    style="width: 100%; height: 500px"
+    :center="center"
+    :zoom="15"
+  >
     <Marker :options="{ position: center }" />
   </GoogleMap>
 </template>
 
 <script lang="ts" setup>
-import { GoogleMap, Marker } from 'vue3-google-map';
+import { GoogleMap, Marker } from "vue3-google-map";
 
 const center = { lat: 46.94849280588769, lng: 7.44748922864467 };
 
