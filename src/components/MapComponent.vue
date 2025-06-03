08d6adc996032823f11380ff5bc4d83b6fd2d950
Bastian Nanchen <bastian.nanchen@cloud.com>
2025-06-03 15:58:32 +0200

    Improve

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,18 +1,14 @@
 <template>
-  <GoogleMap
-    :api-key="apiKey"
-    style="width: 100%; height: 500px"
-    :center="center"
-    :zoom="15"
-  >
-    <Marker :options="{ position: center }" />
+  <GoogleMap :api-key="apiKey" :mapId="mapId" style="width: 100%; height: 500px" :center="center" :zoom="15">
+    <AdvancedMarker :options="{ position: center }" />
   </GoogleMap>
 </template>
 
 <script lang="ts" setup>
-import { GoogleMap, Marker } from "vue3-google-map";
+import { GoogleMap, AdvancedMarker } from 'vue3-google-map';
 
 const center = { lat: 46.94849280588769, lng: 7.44748922864467 };
 
 const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+const mapId = import.meta.env.VITE_GOOGLE_MAPS_MAP_ID;
 </script>
